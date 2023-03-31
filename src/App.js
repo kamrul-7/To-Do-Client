e93f2d4f4a87af57d7c6a4b265e34b28a5354238
kamrul-7 <kamrul7.khan@gmail.com>
2023-04-01 04:37:24 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,73 +1,18 @@
-import React from "react";
-import Tasks from "./Tasks";
+import './App.css';
 
-import "./App.css";
-import { Button, Checkbox, Paper, TextField } from "@mui/material";
+// components
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+import Todos from './components/Todos';
 
-class App extends Tasks {
-    state = { tasks: [], currentTask: "" };
-    render() {
-        const { tasks } = this.state;
-        return (
-            <div className="App flex">
-                <Paper elevation={3} className="container">
-                    <div className="heading">TO-DO</div>
-                    <form
-                        onSubmit={this.handleSubmit}
-                        className="flex ml-10"
-                        
-                    >
-                        <TextField
-                            variant="outlined"
-                            size="small"
-                            style={{ width: "80%" }}
-                            value={this.state.currentTask}
-                            required={true}
-                            onChange={this.handleChange}
-                            placeholder="Add New TO-DO"
-                        />
-                        <Button
-                            style={{ height: "40px" }}
-                            color="primary"
-                            variant="outlined"
-                            type="submit"
-                        >
-                            Add task
-                        </Button>
-                    </form>
-                    <div>
-                        {tasks.map((task) => (
-                            <Paper
-                                key={task._id}
-                                className="flex task_container"
-                            >
-                                <Checkbox
-                                    checked={task.completed}
-                                    onClick={() => this.handleUpdate(task._id)}
-                                    color="primary"
-                                />
-                                <div
-                                    className={
-                                        task.completed
-                                            ? "task line_through"
-                                            : "task"
-                                    }
-                                >
-                                    {task.task}
-                                </div>
-                                <Button
-                                    onClick={() => this.handleDelete(task._id)}
-                                    color="secondary"
-                                >
-                                    delete
-                                </Button>
-                            </Paper>
-                        ))}
-                    </div>
-                </Paper>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div>
+      <Header />
+      <TodoForm />
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
